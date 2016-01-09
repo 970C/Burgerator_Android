b397be6cc15015961047cc207e61df4f8010b282
Kevin Haro <harok@cwu.edu>
2016-01-09 14:54:56 -0800

    Added layout content to RateActivity

--- Burgerator/app/src/main/java/com/example/luis/burgerator/RateActivity.java
+++ Burgerator/app/src/main/java/com/example/luis/burgerator/RateActivity.java
@@ -1,7 +1,16 @@
 package com.example.luis.burgerator;
 
 import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.ScrollView;
+import android.widget.TextView;
 
 public class RateActivity extends Activity {
 
@@ -9,5 +18,93 @@ public class RateActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rate);
+
+        /*This indented block should come before the onClick listeners before
+        the onClick listeners wont trigger.*/
+        // Adding custom elements to a ScrollView
+        LayoutInflater inflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View v = inflater.inflate(R.layout.activity_rate, null);
+
+        // Find the ScrollView
+        ScrollView sv = (ScrollView) v.findViewById(R.id.restaurantScrollView);
+
+        // Inflate the first box of the scroll view
+        View restaurantView = inflater.inflate(R.layout.activity_rate_scroll_content,null);
+
+
+        // Create a LinearLayout element
+        LinearLayout ll = new LinearLayout(this);
+        ll.setOrientation(LinearLayout.VERTICAL);
+
+        for(int i=0; i<100; i++) {
+            // Add text
+            TextView tv = new TextView(this);
+            tv.setText("my text");
+            tv.setTextColor(Color.BLACK);
+            tv.setHeight(50);
+            ll.addView(tv);
+        }
+
+
+
+        // Add the forms/content to the ScrollView
+        sv.addView(restaurantView);
+
+
+        // Display the view
+        setContentView(v);
+
+
+        // Initializes button views and their onClickListeners
+        // button to go to findABurger
+        Button findABurgerButton = (Button) findViewById(R.id.btn_search_activity);
+        findABurgerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), SearchActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        // button to go to burger_feed
+        Button burgerFeedButton = (Button) findViewById(R.id.btn_feed_activity);
+        burgerFeedButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), FeedActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        // button to go to burger_rating
+        Button burgerRatingButton = (Button) findViewById(R.id.btn_rate_activity);
+        burgerRatingButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), RateActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+        // button to go to top_burgers
+        Button topBurgersButton = (Button) findViewById(R.id.btn_top10_activity);
+        topBurgersButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), Top10Activity.class);
+                startActivity(intent);
+            }
+        });
+
+        // button to go to profile
+        Button profileButton = (Button) findViewById(R.id.btn_profile_activity);
+        profileButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), ProfileActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
