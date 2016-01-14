be9165fc0515d91867d486f4bbfc4883e87dbb22
Jonathan Hammond <designbot9000@gmail.com>
2016-01-14 12:42:16 -0800

    added backend eclipse proj

---
+++ burgeratorBackend/src/burgeratorBackend/Address.java
@@ -0,0 +1,14 @@
+package burgeratorBackend;
+
+public class Address {
+	
+	private String address;
+	private String city;
+	private String state;
+	private int zip;
+
+	public Address(){
+		
+	}
+	
+}
---
+++ burgeratorBackend/src/burgeratorBackend/BurgeratorRunner.java
@@ -0,0 +1,18 @@
+package burgeratorBackend;
+
+public class BurgeratorRunner {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		/*
+		 * Log test
+		 * status: works
+		 */
+		//Log.getInstance().addLog("Test");
+		//Log.getInstance().logConsoleDump();
+		
+
+	}
+
+}
