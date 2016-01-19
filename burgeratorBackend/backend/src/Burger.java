41a4212ee1a81164d8bc812947b11259e81cad2a
Jonathan Hammond <designbot9000@gmail.com>
2016-01-18 21:33:19 -0800

    rebuilt backend project

---
+++ burgeratorBackend/backend/src/Burger.java
@@ -0,0 +1,57 @@
+//package burgeratorBackend;
+
+public class Burger {
+	
+	
+	private String name;
+	private  String imagePath;
+	private BurgerRating rating;
+	private BurgerOptions opts;
+	
+	
+	public Burger(){
+		
+	}
+	
+	
+	
+	public void setName(String _name){
+		this.name = _name;
+	}
+	public String getName(){
+		return this.name;
+	}
+	
+	public void setImagePath(String _imagePath){
+		this.imagePath = _imagePath;
+	}
+	public String getImagePath(){
+		return this.imagePath;
+	}
+	
+	public double getBurgerRating(){
+		double rating = -888;
+		if(this.rating instanceof BurgerRating){
+			
+			rating = this.rating.getRating();
+			
+			//Log.getInstance().addLog("Success, Burger rating exists; rating: " + rating);
+			return rating;
+		}
+		return rating;
+	}
+	
+	public void setBurgerRating(double _rating){
+		this.rating = new BurgerRating(_rating);
+	}
+	
+	public void setBurgerOpts(int _cheese, int _btb, int _prep){
+		this.opts = new BurgerOptions(_cheese, _btb, _prep);
+	}
+	public Burger getBurger(){
+		
+		return this;
+	}
+	
+
+}
---
+++ burgeratorBackend/backend/src/DistanceCalc.java
@@ -0,0 +1,5 @@
+//package burgeratorBackend;
+
+public class DistanceCalc {
+
+}
