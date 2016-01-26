2427e669668f05ba3f52ad6862876815c08e6b07
Jonathan Hammond <designbot9000@gmail.com>
2016-01-26 11:19:08 -0800

    updated burger and burgerfeed

--- burgeratorBackend/backend/src/Burger.java
+++ burgeratorBackend/backend/src/Burger.java
@@ -1,57 +1,81 @@
+import org.json.JSONObject;
+
 //package burgeratorBackend;
 
 public class Burger {
 	
+	private JSONObject json;
 	
-	private String name;
-	private  String imagePath;
-	private BurgerRating rating;
-	private BurgerOptions opts;
-	
+	//private static final User USER = new User();
 	
 	public Burger(){
+		this.json = new JSONObject();
+	}
+	
+	public void setJSON(JSONObject j){
+		this.json = j;
+	}
 		
-	}
-	
-	
-	
-	public void setName(String _name){
-		this.name = _name;
-	}
-	public String getName(){
-		return this.name;
-	}
-	
-	public void setImagePath(String _imagePath){
-		this.imagePath = _imagePath;
-	}
-	public String getImagePath(){
-		return this.imagePath;
-	}
-	
-	public double getBurgerRating(){
-		double rating = -888;
-		if(this.rating instanceof BurgerRating){
-			
-			rating = this.rating.getRating();
-			
-			//Log.getInstance().addLog("Success, Burger rating exists; rating: " + rating);
-			return rating;
+	//return a string value for a particular key if it exists
+	private String getVal(String k){
+		String result = "";
+		try{
+			result = json.getString(k);
+			//return result;
+		}catch (Exception e){
+			System.out.println(e);
 		}
-		return rating;
+		return result;
 	}
 	
-	public void setBurgerRating(double _rating){
-		this.rating = new BurgerRating(_rating);
+	public String getId(){
+		return getVal("burgerId");
+	}
+	public String getRestaurantID(){
+		return getVal("restaurantId");
+	}
+	public String getUserEmail(){
+		return getVal("userEmail");
+	}
+	public String getBurgerName(){
+		return getVal("burgerName");
+	}
+	public String getRating(){
+		return getVal("rating");
+	}
+	public String getImageURL(){
+		return getVal("imageURL");
+	}
+	public String getRestaurantName(){
+		return getVal("restaurantName");
+	}
+	public String getRestaurantImageURL(){
+		return getVal("restaurantImageURL");
+	}
+	public String getRestaurantAddress(){
+		return getVal("restaurantAddress");
+	}
+	public String getRestaurantCity(){
+		return getVal("restaurantCity");
+	}
+	public String getRestaurantZip(){
+		return getVal("restaurantZip");
+	}
+	public String getDate(){
+		return getVal("date");
+	}
+	public String getUserPhoto(){
+		return getVal("userPhoto");
+	}
+	public String getPound(){
+		return getVal("pound");
+	}
+	public String getIsPound(){
+		return getVal("ispound");
 	}
 	
-	public void setBurgerOpts(int _cheese, int _btb, int _prep){
-		this.opts = new BurgerOptions(_cheese, _btb, _prep);
-	}
-	public Burger getBurger(){
-		
-		return this;
-	}
+	
+	
 	
 
 }
--- burgeratorBackend/backend/src/BurgerFeed.java
+++ burgeratorBackend/backend/src/BurgerFeed.java
@@ -1,5 +1,61 @@
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.util.*;
+
 //package burgeratorBackend;
 
 public class BurgerFeed {
+	
+	private JSONObject json;
+	//store individual burger JSONObjects in an array;
+	private Burger[] burgers;
+	
+	//instantiate burgerFeed 
+	private static final BurgerFeed FEED = new BurgerFeed();
+	
+	private BurgerFeed(){
+		this.json = new JSONObject();
+	}
+	
+	//return this instance of burgerFeed
+	public static BurgerFeed instance(){
+		return FEED;
+	}
+	
+	public void setFeed(JSONObject j){
+		//accesses the burgers jsonarray and creates an array of Burger(s)
+		this.json = j.getJSONObject("result").getJSONObject("content");
+		int size = json.getJSONArray("burger").length();
+		int count = 0;
+		
+		burgers = new Burger[size];
+		while(count < size){
+			//creates a Burger JSONObject for each burger in the JSONArray - necessary for specific getters for burger
+			burgers[count].setJSON(json.getJSONArray("burger").getJSONObject(count));
+		}
+		
+	}
+	
+	//return a string value for a particular key if it exists
+	private String getVal(String k){
+		String result = "";
+		try{
+			result = json.getString(k);
+		}catch (Exception e){
+			System.out.println(e);
+			//Log.getInstance().addLog("error " + l + ", " + e + "\n");
+		}
+		return result;
+	}
+	
+	public int size(){
+		return this.burgers.length;
+	}
+	
+	public Burger get(int _index){
+		return burgers[_index];
+	}
+	
 
 }
