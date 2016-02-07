b165e12543550a95d21401dcdf180ea85c7aa3d9
Jonathan Hammond <designbot9000@gmail.com>
2016-02-06 18:45:50 -0800

    removed backend

---
+++ Burgerator/app/src/main/java/burgerator/control/Controller.java
@@ -0,0 +1,8 @@
+package burgerator.control;
+
+/**
+ * Created by Jonathan on 2/6/2016.
+ */
+public class Controller {
+}
+
--- burgeratorBackend/backend/src/UserRank.java
+++
@@ -1,5 +0,0 @@
-//package burgeratorBackend;
-
-public class UserRank {
-
-}
